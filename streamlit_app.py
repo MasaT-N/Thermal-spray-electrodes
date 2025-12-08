c6d82ac23667454bee3b8224dfe2141e11cc5453
Masakazu Takai <masakazu_takai@nagatsu-g.co.jp>
2025-12-08 13:36:59 +0900

    新規アカウント登録時の説明を追記した。

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,16 +21,16 @@ st.set_page_config(
     )
 
 pages = {
+    "各種コンテンツ": [
+        st.Page("main_contents.py", title="溶射電極状況表示",icon="📈"),
+        st.Page("recent_shipments.py", title="最新出荷データ検索", icon="🔍"),
+        st.Page("update_syukka_status.py", title="溶射電極出荷状況更新 (長津専用)", icon="🚚"),
+    ],
     "アカウント管理": [
         st.Page("sign_in.py", title="サインイン／サインアップ",icon="🏠"),
         st.Page("change_username.py", title="ユーザー名の変更",icon="👤"),
         st.Page("password_reset.py", title="パスワードの変更",icon="🔑"),
         st.Page("sign_out.py", title="サインアウト",icon="🚪"),
-    ],
-    "コンテンツ": [
-        st.Page("main_contents.py", title="溶射電極状況表示",icon="📈"),
-        st.Page("recent_shipments.py", title="最新出荷データ検索", icon="🔍"),
-        st.Page("update_syukka_status.py", title="溶射電極出荷状況更新", icon="🚚"),
     ],
 }
 
